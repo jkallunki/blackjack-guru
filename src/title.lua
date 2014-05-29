1a107930848ef8051d6055ab485ece99a5c24479
Janne Kallunki <janne@kallunki.org>
2014-05-29 16:38:38 +0300

    increased title size

--- src/title.lua
+++ src/title.lua
@@ -2,5 +2,5 @@ Title = Label:subclass('Title')
 
 function Title:initialize(params)
    Label.initialize(self, params)
-   self.font = love.graphics.newFont("media/fonts/blanch.otf", 60)
+   self.font = love.graphics.newFont("media/fonts/blanch.otf", 66)
 end
