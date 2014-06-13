5cc2cee6a94354944dae5acfd89cc1d78d3d0e61
Janne Kallunki <janne@kallunki.org>
2014-06-14 00:13:37 +0300

    card implementation without canvas

--- src/card.lua
+++ src/card.lua
@@ -7,39 +7,34 @@ function Card:initialize(params)
    Node.initialize(self, params)
 
    self.suitImage = love.graphics.newImage("media/images/" .. self.suit .. ".png")
-   self.canvas = love.graphics.newCanvas(128, 256)
-   love.graphics.setCanvas(self.canvas)
-   self.canvas:clear()
+   self.font = love.graphics.newFont("media/fonts/blanch.otf", 40)
+   self.dim = false
+end
+
+function Card:draw()
    love.graphics.setBlendMode('alpha')
-   love.graphics.setColor({255,255,255,255})
-   love.graphics.rectangle("fill", 0, 0, 100, 140)
+   love.graphics.push()
+   love.graphics.translate(self:getX() + 50, self:getY() + 70)
+   love.graphics.rotate(self.angle)
+   if self.dim then
+      love.graphics.setColor({192,192,192,255})
+   else
+      love.graphics.setColor({255,255,255,255})
+   end
+   love.graphics.rectangle("fill", -50, -70, 100, 140)
    love.graphics.setLineWidth(1)
    love.graphics.setColor({0,0,0,255})
-   love.graphics.rectangle("line", 0, 0, 100, 140)
-   love.graphics.setFont(love.graphics.newFont("media/fonts/blanch.otf", 40))
-
+   love.graphics.rectangle("line", -50, -70, 100, 140)
+   love.graphics.setFont(self.font)
    if self.suit == 'spades' or self.suit == 'clubs' then
       love.graphics.setColor({0,0,0,255})
    else
       love.graphics.setColor({189,36,36,255})
    end
-   love.graphics.printf(self.value, 0, 5, 30, 'center')
+   love.graphics.printf(self.value, -50, -65, 30, 'center')
    love.graphics.setColor({255,255,255,255})
-   love.graphics.draw(self.suitImage, 26, 64, 0, 0.5, 0.5, 0, 0)
-
-   love.graphics.draw(self.suitImage, 5, 45, 0, 0.16, 0.16, 0, 0)
-   love.graphics.setCanvas()
-
-   self.dim = false
-end
-
-function Card:draw()
-   love.graphics.setBlendMode('premultiplied')
-   if self.dim then
-      love.graphics.setColor({96,96,96,255})
-   else
-      love.graphics.setColor({255,255,255,255})
-   end
-   love.graphics.draw(self.canvas, self:getX() + 50, self:getY() + 70, self.angle, 1, 1, 50, 70)
+   love.graphics.draw(self.suitImage, -24, -6, 0, 0.5, 0.5, 0, 0)
+   love.graphics.draw(self.suitImage, -45, -25, 0, 0.16, 0.16, 0, 0)
    love.graphics.setBlendMode('alpha')
+   love.graphics.pop()
 end
